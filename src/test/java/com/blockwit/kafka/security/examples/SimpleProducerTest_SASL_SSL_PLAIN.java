b4b88494eaadbae96d5e31150a5c8f5ee9e91bf4
Alexander Strakh <astrakh@PMINTL.NET>
2020-11-20 22:01:16 +0300

    Initial commit

--- src/test/java/com/blockwit/kafka/security/examples/SimpleProducerTest_SASL_SSL_PLAIN.java
+++ src/test/java/com/blockwit/kafka/security/examples/SimpleProducerTest_SASL_SSL_PLAIN.java
@@ -4,7 +4,7 @@ public class SimpleProducerTest_SASL_SSL_PLAIN {
 
     public static void main(String[] args) throws InterruptedException {
         SimpleProducer.runProducer(Helper.of(
-                "sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required serviceName=\"Kafka\" username=\"admin\" password=\"admin-secret\";",
+                "sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required serviceName=\"Kafka\" username=\"alice\" password=\"alice-secret\";",
                 "security.protocol", "SASL_PLAINTEXT",
                 "sasl.mechanism", "PLAIN"),
                 "localhost:9093", "test.topic");
